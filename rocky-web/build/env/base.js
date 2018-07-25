f2de0fde8277a962ffa1f8567650a0e592e997f9
johnzhu12 <1042532367@qq.com>
2018-07-25 11:47:26 +0800

    update

--- rocky-web/build/env/base.js
+++ rocky-web/build/env/base.js
@@ -1,14 +1,19 @@
 let path = require('path');
 let webpack = require('webpack');
+const rootDir = process.cwd() + "/rocky-web/" //跟地址
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: process.cwd() + "/rocky-web/app/main.js",//已多次提及的唯一入口文件
+    entry: rootDir + "app/main.js",//已多次提及的唯一入口文件
     output: {
-        path: process.cwd() + "/rocky-web/dist",//打包后的文件存放的地方
+        path: rootDir + "dist",//打包后的文件存放的地方
         filename: "bundle.js"//打包后输出文件的文件名
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()//热加载
+        new webpack.HotModuleReplacementPlugin(),//热加载
+        new HtmlWebpackPlugin({
+            template: rootDir + "app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+        })
     ],
     module: {
         rules: [{
@@ -21,7 +26,7 @@ module.exports = {
                     ]
                 }
             },
-            include: process.cwd() + "/rocky-web/app/",
+            include: rootDir + "app/",
             exclude: /node_modules/
         },
         {
